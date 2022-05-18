5ad37369d8b2013a98a2899ffc300d7fd1264fff
fabioportovasques <fabioportovasques321@gmail.com>
2022-05-17 23:59:02 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1" />    
+        <title>óleo Service</title>
+        <link rel="stylesheet" type="text/css" href="assets/css/style2.css">
+        <!-- fontes -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600;700&display=swap" rel="stylesheet">
+        <!-- link para icones -->
+    <script src="https://kit.fontawesome.com/0518f3ccf0.js" crossoring="anonymous"> </script>
+
+    </head>
+
+    <header>       
+            <div class="logo">
+                <img src="assets/images/fundo2.png" />
+            </div>
+            <div class="logout">
+                Bem vindo:<span> Fábio Vasques</span>
+            </div>       
+    </header>
+    <main>
+        <div class="containner">
+                <section class="menu">
+                    <nav>
+                        <ul>
+                            <li><a href="">home</a></li>
+                            <li><a href="">home</a></li>
+                            <li><a href="">home</a></li>
+                            <li><a href="">home</a></li>
+                            <li><a href="">home</a></li>
+
+                        </ul>
+                    </nav>                                        
+                </section>
+                <section class="containner-cards">                 
+                    <section class="card">                                
+                        <div class="sub-card-trocas">
+                        <div class="subCard-icone">
+                                <i class="fa-solid fa-check"></i>
+                        </div>
+                            <div class="subCard-texto">
+                                Trocas realizadas
+                            </div>
+                    </div>
+                    </section>
+                    <section class="card">                                
+                        <div class="sub-card-vencer">
+                        <div class="subCard-icone">
+                                <i class="fa-solid fa-bell-slash"></i>
+                        </div>
+                            <div class="subCard-texto">
+                                Trocas a Vencer
+                            </div>
+                    </div>
+                    </section>
+                    <section class="card">                                
+                        <div class="sub-card-vencido">
+                        <div class="subCard-icone">
+                                <i class="fa-solid fa-circle-exclamation"></i>
+                        </div>
+                            <div class="subCard-texto">
+                                Trocas Vencidas
+                            </div>
+                        </div>
+                    </section>
+                </section>              
+                                                                
+        </div>       
+    </main>
+
+
+</html>
